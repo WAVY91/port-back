ae2549bf7934cad82356413a833d9001ad785fe1
WAVY91 <cena@github.com>
2025-12-26 13:31:44 +0100

    keke

--- config/email.js
+++ config/email.js
@@ -2,12 +2,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.ADMIN_EMAIL,
     pass: process.env.ADMIN_PASSWORD,
   },
+  connectionTimeout: 5000,
+  socketTimeout: 5000,
 });
 
 const sendUserConfirmation = async (contactData) => {
